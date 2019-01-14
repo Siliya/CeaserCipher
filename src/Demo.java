07df2656cd3afe1b931e45fe80e96788ec732d63
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-01-14 21:15:25 +0200

    correction

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
-        char alphabetEn = {a, b, c, d, e, f, g, h, i, g, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+        char [] alphabetEn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
 
 
     }
