7987d13124602c8d5dcaea1d5ed94f177806977a
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-01-15 23:11:43 +0200

    new method

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,22 @@
 public class Demo {
+    //TODO formula  encrypt n + 3 - t = q decrypt n - 3 q=t or the other way around
     public static void main(String[] args) {
         char [] alphabetEn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+       // alphabetEn [0] = 1;
+       // alphabetEn [1] = 2;
 
+    }
+    public static String encryptCeaser (String text, int encryption){
+        if (encryption>26){
+            encryption% 26;}
+        else if (encryption<0){
+            encryption =(encryption%26)+26;
+            String cipher = "";
+           
+
+
+        }
 
     }
 }
