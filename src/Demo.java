4a6dd6d2663f36667ab9c3de3cdad7ffacd3a099
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-01-30 18:44:37 +0200

    "ver. 1 = cond. statements + loop && comments"

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     //TODO formula  encrypt n + 3 - t = q decrypt n - 3 q=t or the other way around
     //TODO първо си правя шифровката, за да го дешифровам, а и да изпробвам различни варианти
     public static void main(String[] args) {
-        char [] alphabetEn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    //    char [] alphabetEn = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
        // alphabetEn [0] = 1;
        // alphabetEn [1] = 2;
@@ -10,12 +10,12 @@ public class Demo {
     }
 
     //С условни конструкции и цикли
-    public static String encryptCeaser (String text, int encryption){
-        if (encryption>26){
+    public static String encryptCeaser (String text, int swift){
+        if (swift>26){
 
         }
-        else if (encryption<0){
-            encryption =(encryption%26)+26;
+        else if (swift <0){
+            swift =(swift%26)+26;
             String cipher = "";
             int lenght = text.length();
             for (int i = 0; i < lenght; i++) {
