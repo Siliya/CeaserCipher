d3ecdba448f38c381ed9755ec4b0f15bdc9b34e8
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-01-31 16:57:57 +0200

    "new method"

--- src/Demo2.java
+++ src/Demo2.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class Demo2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your encrypted message");
+        String enMessage = input.nextLine();
+        System.out.println();
 
     }
-    public static String CeaserDecryption (){
+
+
+    public static String CeaserDecryption (String text, int shift){
+        char[] alphabet =  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z'};
+        for (char i:alphabet){
+            System.out.println(i);
+        }
+        String cipher = "";
+        return cipher;
 
     }
+
 }
