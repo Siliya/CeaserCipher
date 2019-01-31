2122358b2d9e78380ef9c77324a99683c85421bf
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-01-31 16:35:40 +0200

    "new scanner"

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Demo2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+    }
+    public static String CeaserDecryption (){
+
+    }
 }
