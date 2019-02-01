9195484100137ef641a8779bfebf32bd9cce4dbf
Siliya Ivanova <45152228+Siliya@users.noreply.github.com>
2019-02-02 00:05:28 +0200

    comments

--- src/DecryptionMostFrequant.java
+++ src/DecryptionMostFrequant.java
@@ -1,16 +1,16 @@
-public class DecryptionMostFrequant
-//TODO ТОВА ТРЯБВА ДА Е ОКОНЧАТЕЛНАТА ВЕРСИЯ С e t h most frequent
-{
+public class DecryptionMostFrequant {
+    //TODO ТОВА ТРЯБВА ДА Е ОКОНЧАТЕЛНАТА ВЕРСИЯ С e t h most frequent
+
     public static void main(String[] args) {
         String enMessage = "cneg bs gur wbhearl vf gur raq";
-        enMessage = enMessage.toLowerCase();
-        char[] messageArray = enMessage.trim().toCharArray();
-        char mostFrequent = ' ';
+        enMessage = enMessage.toLowerCase(); //връща всички char към малки букви
+        char[] messageArray = enMessage.trim().toCharArray(); //реже пространствата и разделя масива на  char-ове
+        char mostFrequent = ' '; //три променливи, които използвам, едната временна, за да изчисля и завъртя стойностите им по-долу
         int temp = 0;
         int bestNumber = 0;
-        for (int i = 0; i < messageArray.length; i++) {
+        for (int i = 0; i < messageArray.length; i++) { //обхождам масива
             if (messageArray[i] != 'e' && messageArray[i] != 't' && messageArray[i] != 'h' && messageArray[i] != 'e') {
-                temp++;
+                temp++; //проверявам за най-често срещаните букви и му задавам какво да прави, ако не са e t h
                 if (temp > bestNumber) {
                     bestNumber = temp;
                     mostFrequent = messageArray[i];
@@ -24,14 +24,10 @@ public class DecryptionMostFrequant
 
     public static void ShiftAndPrint(char[] allLetters, char letter) {
         for (int i = 0; i < allLetters.length; i++) {
-            System.out.println(i);
+            System.out.println(i);  //обхождам масива, за да го принтирам
 
         }
 
     }
 
 }
-
-
-
-
