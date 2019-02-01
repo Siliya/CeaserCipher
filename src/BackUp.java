50308cd8d477d241333e26acaae3900c76c75563
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-02-01 15:53:43 +0200

    backup process

---
+++ src/BackUp.java
@@ -0,0 +1,34 @@
+public class BackUp {
+        public static void main(String[] args) {
+            String enMessage = "cneg bs gur wbhearl vf gur raq";
+            char [] mostOccuring = enMessage.trim().toCharArray();
+            char mostFreqent;
+            int temp =0;
+            int bestNumber =0;
+
+
+            // char [] mostOccuring = {'e', 't', 'h'};
+
+            enMessage = enMessage.toLowerCase();
+            for (int i = 0; i < mostOccuring.length; i++) {
+                System.out.println(i);
+
+
+            }
+
+        }
+        public static void MostFrequent (char [] mostFrequent, String enMessage){
+            enMessage = enMessage.trim().toLowerCase();
+            char [] mostFreqeunt = enMessage.toCharArray();
+            for (int i = 0; i <mostFreqeunt.length ; i++) {
+                System.out.println(i);
+
+            }
+
+
+
+
+        }
+
+    }
+
---
+++ src/TryOut.java
@@ -0,0 +1,34 @@
+public class TryOut {
+
+        public static void main(String[] args) {
+            String enMessage = "cneg bs gur wbhearl vf gur raq";
+            enMessage = enMessage.toLowerCase();
+            char [] messageArray = enMessage.trim().toCharArray();
+            char  mostFrequent = ' ';
+            int temp =0;
+            int bestNumber =0;
+
+
+            for (int i = 0; i < messageArray.length; i++) {
+                if (messageArray[i] != 'e' && messageArray[i] != 't' && messageArray[i] != 'h' && messageArray[i] != 'e' ) {
+                    temp++;
+                    if (temp > bestNumber) {
+                        bestNumber = temp;
+                        mostFrequent = messageArray[i];
+                        temp = 0;
+                    }
+                }
+            }
+            ShiftAndPrint(messageArray, mostFrequent);
+        }
+        public static void ShiftAndPrint (char [] allLetters, char  letter){
+
+            for (int i = 0; i <allLetters.length ; i++) {
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
