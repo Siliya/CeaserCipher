f470154fa62de260859e037cd5330a9cf32959cf
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-02-01 14:55:24 +0200

    changes

--- src/DecryptionMostFrequant.java
+++ src/DecryptionMostFrequant.java
@@ -4,30 +4,30 @@ public class DecryptionMostFrequant
     public static void main(String[] args) {
         String enMessage = "cneg bs gur wbhearl vf gur raq";
         char [] mostOccuring = enMessage.trim().toCharArray();
-        char [] messageArray = enMessage.split(enMessage.toCharArray("")); //na kakvo go splitvam?
-
-       // char [] mostOccuring = {'e', 't', 'h'};
-
-
-
-        enMessage = enMessage.toLowerCase();
-        for (int i = 0; i < mostOccuring.length; i++) {
-
-
-        }
-
-    }
-    public static void MostFrequent (char [] mostFrequent, String message){
-        message =message.trim().toLowerCase();
-        char [] mostFreqeunt = message.toCharArray();
         char mostFreqent;
         int temp =0;
         int bestNumber =0;
-        for (i:mostFreqeunt) {
-            System.out.println();
+
+
+       // char [] mostOccuring = {'e', 't', 'h'};
+
+        enMessage = enMessage.toLowerCase();
+        for (int i = 0; i < mostOccuring.length; i++) {
+            System.out.println(i);
 
 
         }
+
+    } 
+    public static void MostFrequent (char [] mostFrequent, String enMessage){
+        enMessage = enMessage.trim().toLowerCase();
+        char [] mostFreqeunt = enMessage.toCharArray();
+        for (int i = 0; i <mostFreqeunt.length ; i++) {
+            System.out.println(i);
+
+        }
+
+
 
 
     }
