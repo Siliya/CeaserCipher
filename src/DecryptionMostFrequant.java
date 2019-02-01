b8b8c2278380ac353debe0cd0ab96d4fa7fe35b8
Siliya Ivanova <siliya.ivanova@gmail.com>
2019-02-01 16:23:09 +0200

    shift and print

--- src/DecryptionMostFrequant.java
+++ src/DecryptionMostFrequant.java
@@ -4,12 +4,12 @@ public class DecryptionMostFrequant
     public static void main(String[] args) {
         String enMessage = "cneg bs gur wbhearl vf gur raq";
         enMessage = enMessage.toLowerCase();
-        char [] messageArray = enMessage.trim().toCharArray();
-        char  mostFrequent = ' ';
-        int temp =0;
-        int bestNumber =0;
+        char[] messageArray = enMessage.trim().toCharArray();
+        char mostFrequent = ' ';
+        int temp = 0;
+        int bestNumber = 0;
         for (int i = 0; i < messageArray.length; i++) {
-            if (messageArray[i] != 'e' && messageArray[i] != 't' && messageArray[i] != 'h' && messageArray[i] != 'e' ) {
+            if (messageArray[i] != 'e' && messageArray[i] != 't' && messageArray[i] != 'h' && messageArray[i] != 'e') {
                 temp++;
                 if (temp > bestNumber) {
                     bestNumber = temp;
@@ -18,16 +18,20 @@ public class DecryptionMostFrequant
                 }
             }
         }
-
+        ShiftAndPrint(messageArray, mostFrequent);
 
     }
-    public static void ShiftAndPrint (char [] allLetters, char  letter){
+
+    public static void ShiftAndPrint(char[] allLetters, char letter) {
+        for (int i = 0; i < allLetters.length; i++) {
+            System.out.println(i);
 
         }
-
-
-
 
     }
 
 }
+
+
+
+
